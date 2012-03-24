e0c014b5398d2ee71ba54e1b78a026931abcd073
pete <p.reisinger@gmail.com>
2012-03-24 14:22:27 +0000

    added ItemInfoService - communicates with rest service

--- XcubeNFC/src/org/xcube/nfc/XcubeNFCActivity.java
+++ XcubeNFC/src/org/xcube/nfc/XcubeNFCActivity.java
@@ -1,12 +1,15 @@
 package org.xcube.nfc;
 
+import org.xcube.nfc.domain.Item;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 public class XcubeNFCActivity extends Activity {
 
-    private static final String COUNT_LABEL = "count";
+    private static final String COUNT_LABEL = "";
     private static final String ITEM_LABEL = "item";
     private static final String CALORIES_LABEL = "calories";
     private static final String PRICE_LABEL = "price";
@@ -16,6 +19,24 @@ public class XcubeNFCActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setMainView();
+        addItem(null);
+    }
+
+    public void addItem(Item item) {
+
+        TableRow itemsTableRow = (TableRow) findViewById(R.id.items);
+
+        itemsTableRow.addView(getTextView("1"));
+        itemsTableRow.addView(getTextView("beef"));
+        itemsTableRow.addView(getTextView("300"));
+        itemsTableRow.addView(getTextView("2.99"));
+    }
+
+    public TextView getTextView(String text) {
+
+        TextView textView = new TextView(this);
+        textView.setText(text);
+        return textView;
     }
 
     private void setMainView() {
